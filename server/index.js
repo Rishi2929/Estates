ed61e651e2256ee9425bc490cf7726f937811ff4
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2024-04-25 14:25:32 +0530

    database connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 import express from "express"
+import mongoose, { Mongoose, mongo } from "mongoose"
+import dotenv from "dotenv"
+import { connectDB } from "./data/database.js";
+import { config } from "dotenv";
+
+
+config({
+    path: "./data/.env",
+});
+
+connectDB();
 
 const app = express()
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
+
+
