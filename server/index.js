12dd1e19cd4a688085e0acad544e2aa310b8bdda
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2024-04-25 18:00:35 +0530

    signup api created

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import mongoose, { Mongoose, mongo } from "mongoose"
 import dotenv from "dotenv"
 import { connectDB } from "./data/database.js";
 import { config } from "dotenv";
-
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 config({
     path: "./data/.env",
@@ -12,9 +13,13 @@ config({
 connectDB();
 
 const app = express()
+app.use(express.json());
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
 
 
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
